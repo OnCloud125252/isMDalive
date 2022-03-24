3b2c0826ec7d5767b9c8ce1fc91eb4eb0c7d19ef
Anonymous-AAAA <75195127+Anonymous-AAAA@users.noreply.github.com>
2022-03-24 23:21:31 +0800

    Update-website
    Create chart

--- website/src/functions/function.js
+++ website/src/functions/function.js
@@ -1,5 +1,5 @@
-import prettyMS from 'pretty-ms';
 import fetch from 'node-fetch';
+import prettyMS from 'pretty-ms';
 
 export function TWtime() {
     let date = new Date();
@@ -18,14 +18,76 @@ export function TWtime() {
     }
 }
 
-export function api() {
-    fetch("http://localhost:3000/api", {
-        "headers": null,
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": {
-            "name": "alex",
-            "age": 15
+export async function api(path) {
+    return await fetch(`http://localhost:8000/api/${path}`).then(response => { return response.json(); })
+}
+
+export function chart(par) {
+    new Chart(par.ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: par.data
+            }]
         },
-        "method": "GET"
-    })
+
+        // Configuration options go here
+        options: {
+            title: {
+                display: true,
+                text: 'Demo Line Chart'
+            },
+            legend: {
+                labels: {
+                    fontColor: 'rgb(0, 0, 0)',
+                    usePointStyle: true
+                }
+            },
+            scales: {
+                // 各軸標題設定
+                // https://www.chartjs.org/docs/latest/axes/labelling.html
+                // 各軸格線設定
+                // https://www.chartjs.org/docs/latest/axes/styling.html
+                // x 軸設置
+                xAxes: [{
+                    // x 軸標題
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Mouth",
+                        fontSize: 16
+                    },
+                    // x 軸格線
+                    gridLines: {
+                        display: true
+                    }
+                }],
+                // y 軸設置
+                yAxes: [{
+                    // y 軸標題
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Amount",
+                        fontSize: 16
+                    },
+                    // y 軸格線
+                    gridLines: {
+                        display: false
+                    },
+                    // y 軸間距
+                    ticks: {
+                        min: 0,
+                        max: 120,
+                        stepSize: 30
+                    }
+                }]
+            }
+        }
+    });
 }
--- website/src/index.js
+++ website/src/index.js
@@ -1,14 +1,16 @@
 import * as func from "./functions/function.js";
 
-
-//console.log("123");
-let homeChart = document.getElementById("home").getContext("2d");
-
 let a = document.getElementById("text");
-let b = document.getElementById("")
+//let b = document.getElementById("")
 
-setInterval(() => {
-    a.innerText = func.TWtime().full;
-    //b.innerText = func.api();
-}, 500);
+const ctxz = document.getElementById('home').getContext('2d');
 
+let input = {
+    ctx: document.getElementById('home').getContext('2d'),
+    data: [0, 30, 45, 28, 60, NaN, 115]
+};
+
+func.chart(input);
+func.api('home').then(response => {
+    console.log(response);
+});
